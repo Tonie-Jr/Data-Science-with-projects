712cb59600e130cc4518a85a32c81b8312d8cbc7
Tonnie <antonymuriithi505@gmail.com>
2025-01-07 11:04:06 +0300

    jason file recap

--- Testingfile.py
+++ Testingfile.py
@@ -1,38 +1,15 @@
+import json
 import pandas as pd
-from pandas.core.construction import range_to_ndarray
+info = """{"column1": [1, 2, 3, 4, 5], "column2": [66.3, 54.43, 543.4, 543.6, 54334], "column3": [6, 3, 7, 32, 7]}"""
+print(type(info))
 
-with open("D:\\Study\\WorldQuat Data Science\\Data\\ts.csv", "r") as f:
-    colum_names = f.readline().strip().split(",")
-    data = []
-    for j in f:
-        tt = j.strip().split(",")
+info1 = json.loads(info) #Load json string into python dictionary
+print(type(info1))
+info2 = json.dumps(info1) #Convert the python dictionary into jason string
+print(type(info2))
 
-        if len(tt) == len(colum_names):
-            data.append(tt)
-df = {colum_names[i]:[item[i] for item in data] for i in range(len(colum_names))}
-df = pd.DataFrame(df)
-print(df)
-print(df.head())
+dataframe1 = pd.DataFrame.from_dict(info1, orient= "index")
+print(dataframe1)
 
 
-#Dictionary comprehension
-keys = ["a", "b", "c", "d", "e"]
-values = [1, 2, 3, 4, 5]
-my_dict = {k:v for (k,v) in zip(keys, values)} # Using the help of the zip() function
 
-
-my_dict1 = {keys[i] : values[i] for i in range(len(keys)) }
-print(my_dict1)
-my_dict2 = {keys[i] : values[i] for i in values for i in range(len(keys)) }
-print(my_dict2)
-
-#Dictionary comprehension using a condition using if statement
-new_dict = {x:x**2 for x in range(20) if x**2 % 4 != 0}
-print(new_dict)
-
-new_dict1 = {x:x**2 for x in range(20+1) if x**2 % 4 == 0}
-print(new_dict1)
-
-m = "TON"
-newdict = {x : {y : x for y in m} for x in m}
-print(newdict)
--- WQuant.py
+++ WQuant.py
@@ -245,3 +245,14 @@ print(new_dict1)
 m = "TON"
 newdict = {x : {y : x for y in m} for x in m}
 print(newdict)
+
+info = """{"column1": [1, 2, 3, 4, 5], "column2": [66.3, 54.43, 543.4, 543.6, 54334], "column3": [6, 3, 7, 32, 7]}"""
+print(type(info))
+
+info1 = json.loads(info) #Load json string into python dictionary
+print(type(info1))
+info2 = json.dumps(info1) #Convert the python dictionary into jason string
+print(type(info2))
+
+dataframe1 = pd.DataFrame.from_dict(info1, orient= "index")
+print(dataframe1)
