1f17a79d8e97352247f6297a963d75b67f17e337
Tonnie <antonymuriithi505@gmail.com>
2025-01-08 23:46:50 +0300

    Working with dataframe

--- Testingfile.py
+++ Testingfile.py
@@ -1,10 +1,9 @@
-column_row = ["a", "b", "c", "d"]
-row01 = [6, 7, 90, 84]
-row02 = [77, 988, 760, 7788]
-#Dictoionary comprehension
-pickle_dict = {m:n for (m,n) in zip(column_row, row02)}
-print(pickle_dict)
 
-import pickle
-
-pickle.dumps(pickle_dict, open("D:\Study\WorldQuat Data Science\Data\pickle files\pickle_dict.pkl", "wr"))
+import pandas as pd
+data = pd.read_csv("D:\\Study\\WorldQuat Data Science\\Data\\brasil-real-estate-2.csv", encoding = "latin")
+#print(data.head()) #print the first five rows of the data.
+print(data.index[:5]) #This retrieves the index of the DataFrame, which is essentially the row labels. By default, the index is an integer-based range starting from 0. [:5] Slices notation selects the first five elements of the index.
+print(data.shape) #To tell the data shape of the dataframe(the number of rows and columns).
+print(data.head()) #Print the first five rows of the data.
+print(data.loc[2]) #print the third row of the data
+print(data["state"])
--- WQuant.py
+++ WQuant.py
@@ -199,7 +199,7 @@ import pandas as pd
 #Sorting Even though the DataFrame in many ways behaves similarly to a dict, it also is ordered. Therefore, we can sort the data in it. Pandas provides two sorting methods, sort_values and sort_index.
 
 data1 = pd.read_csv("D:\\Study\\Rapic Tech Skills\\Data Science\\worldometer_data.csv")
-data2 = pd.read_csv(r"D:\\Study\Rapic Tech Skills\\Data Science\\worldometer_data.csv")
+data2 = pd.read_csv(r"D:\\Study\\Rapic Tech Skills\\Data Science\\worldometer_data.csv")
 data3 = pd.read_csv(r"D:/Study/Rapic Tech Skills/Data Science/worldometer_data.csv")
 data4 = pd.read_csv("D:/Study/Rapic Tech Skills/Data Science/worldometer_data.csv")
 print(data1.head())
@@ -258,3 +258,13 @@ print(type(info2))
 
 dataframe1 = pd.DataFrame.from_dict(info1, orient= "index")
 print(dataframe1)
+
+
+import pandas as pd
+data = pd.read_csv("D:\\Study\\WorldQuat Data Science\\Data\\brasil-real-estate-2.csv", encoding = "latin")
+#print(data.head()) #print the first five rows of the data.
+print(data.index[:5]) #This retrieves the index of the DataFrame, which is essentially the row labels. By default, the index is an integer-based range starting from 0. [:5] Slices notation selects the first five elements of the index.
+print(data.shape) #To tell the data shape of the dataframe(the number of rows and columns).
+print(data.head()) #Print the first five rows of the data.
+print(data.loc[2]) #print the third row of the data
+print(data["state"])
